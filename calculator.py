9626149cfeacadceb2e58ccc5e536b3130ed9d09
Adjocacher Abednego <141531265+abednegoadjocacher@users.noreply.github.com>
2025-05-04 23:57:37 +0000

    Calculator

---
+++ calculator.py
@@ -0,0 +1,72 @@
+def add(x,y):
+    return x + y
+
+def division(x,y):
+    if y == 0:
+        print("A number can not be divide by 0")
+    return x / y
+
+
+def subtract(x,y):
+    return x - y
+
+
+def multiple(x,y):
+    return x * y
+
+
+def modulo(x,y):
+    return x % y
+
+
+
+
+def main():
+    print("""
+        Selection your operation
+        1. Addition
+        2. Division
+        3. Subtraction
+        4. Multiplication
+        5. Modulo
+    """)
+
+    while True:
+        try:
+            option = int(input("Select from (1-5)"))
+
+            first_num = int(input("Enter first number: "))
+            second_num = int(input("Enter second number: "))
+
+            if option == 1:
+                print(f"The sum of {first_num} and {second_num} is : ", add(first_num, second_num))
+
+
+            elif option == 2:
+                print(f"The Division of {first_num} and {second_num} is : ", division(first_num, second_num))
+
+            elif option == 3:
+                print(f"The difference of {first_num} and {second_num} is : ", subtract(first_num, second_num))
+
+            elif option == 4:
+                print(f"The product of {first_num} and {second_num} is : ", multiple(first_num, second_num))
+
+            elif option == 5:
+                print(f"The modulo of {first_num} and {second_num} is : ", modulo(first_num, second_num))
+
+            else:
+                print("No such option!!!")
+        except ValueError:
+            print("❌❌Invalid input!!")
+
+
+        again = input("Do you want too quit? (Yes Or No)")
+        if again.lower().startswith("n"):
+            break
+        # elif again.lower() is not "n" or "y": I have some bug tto solve here.
+        #     print("Sorry Incorrect response")
+        #     return
+        else:
+            main()
+
+main()
